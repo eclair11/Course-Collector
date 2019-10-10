1f70a23126f2225b0e720dc5dc9ee77255b11ec2
Elias Romdan <elias.romdan@etu.univ-st-etienne>
2019-10-10 16:04:31 +0200

    Premiére page des cours

--- src/src/main/java/org/coursecollector/esi/MainController.java
+++ src/src/main/java/org/coursecollector/esi/MainController.java
@@ -1,14 +1,15 @@
 package org.coursecollector.esi;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * CollectorController
- */
 @Controller
 public class MainController {
 
-    
+    @RequestMapping("/cours")
+    public String ListeDesCours(Model model) {
+        return "cours";
+    }
     
 }
