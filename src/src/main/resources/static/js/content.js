09dfa8addfed7c7a1c6103aab567cdfbf4cff63e
Elias Romdan <elias.romdan@etu.univ-st-etienne>
2019-10-11 14:30:00 +0200

    Réorganisation page de contenu des cours

--- src/src/main/resources/static/js/content.js
+++ src/src/main/resources/static/js/content.js
@@ -13,6 +13,18 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+var req = document.getElementById("request-button");
+
+var sha = document.getElementById("share-button");
+
+var reqmod = document.getElementById("request-modal");
+
+var shamod = document.getElementById("share-modal")
+
+var reqclose = document.getElementById("request-close");
+
+var shaclose = document.getElementById("share-close");
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
@@ -23,10 +35,31 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+req.onclick = function() {
+  reqmod.style.display = "block";
+}
+
+sha.onclick = function() {
+  shamod.style.display = "block";
+}
+
+reqclose.onclick = function() {
+  reqmod.style.display = "none";
+}
+
+shaclose.onclick = function() {
+  shamod.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  else if (event.target == reqmod) {
+    reqmod.style.display = "none";
+  }
+  else if (event.target == shamod) {
+    shamod.style.display = "none";
+  }
 }
-
