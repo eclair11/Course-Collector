19d0eda611afd7ccf055ef6b261195a73e405f1e
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2019-10-14 23:08:46 +0200

    ajout: bouton supprimer et modifier page admin

--- src/src/main/resources/static/js/modals.js
+++ src/src/main/resources/static/js/modals.js
@@ -4,36 +4,58 @@
  * and open the template in the editor.
  */
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-const btnElts = document.querySelectorAll(".myBtn");
+function handleModals(btnClassName, modalIdPrefix) {
+    // Get the button that opens the modal
+    const btnElts = document.querySelectorAll("." + btnClassName);
 
 // when the user clicks one of the button open the modal that correspond to the button
-for (let i = 0; i < btnElts.length; i++) {
-    btnElts[i].onclick = function () {
-        // get id of the correspondant modal
-        const modalId = "modal-" + btnElts[i].id
-        // get the modal
-        const modal = document.getElementById(modalId)
-        modal.style.display = "block";
-        // get the span that closes the modal
-        const closerId = "close-" + btnElts[i].id
-        const span = document.getElementById(closerId)
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
-            modal.style.display = "none";
-        }
+    for (let i = 0; i < btnElts.length; i++) {
+        btnElts[i].onclick = function () {
+            // get id of the correspondant modal
+            const modalId = modalIdPrefix + btnElts[i].id
+            // get the modal
+            const modal = document.getElementById(modalId)
+            modal.style.display = "block";
+            // get the span that closes the modal
+            const closerId = "close-" + btnElts[i].id
+            const span = document.getElementById(closerId)
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
 
+        }
     }
 }
+
+/****
+ * 
+ *  MODAL TYPE CONTENUS
+ *
+ */
+
+handleModals("myBtn", "modal-")
+
+
+/**
+ * 
+ * MODAL TYPE DELETE
+ * 
+ */
+handleModals("delBtn", "modal-")
+
+/**
+ * 
+ * MODAL TYPE EDIT
+ * 
+ */
+handleModals("editBtn", "modal-")
 
 
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-   const modals = document.querySelectorAll(".modal");
+    const modals = document.querySelectorAll(".modal");
     for (let i = 0; i < modals.length; i++) {
         if (event.target == modals[i]) {
             modals[i].style.display = "none";
